78ecf1f8a697d1dcdb9aba13cde905206121c317
kundan kumar <kundanjvp123@gmail.com>
2024-03-29 03:39:48 -0700

    of add

--- src/main/java/maven/calculator.java
+++ src/main/java/maven/calculator.java
@@ -12,7 +12,7 @@ public class calculator {
         System.out.print("Enter the and choose the mention operator and operator is (+, -, *, /): ");
         operator = input.next().charAt(0);
 
-        System.out.print("Enter the second number for add minus multiply: ");
+        System.out.print("Enter the second of  number for add minus multiply: ");
         num2 = input.nextDouble();
 
         if (operator == '+') {
