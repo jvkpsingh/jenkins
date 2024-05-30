d3c2f751c07d00d22ed5c61cc0dc432bcb7d99ab
kundan kumar <kundanjvp123@gmail.com>
2024-05-29 21:24:24 -0700

    all

--- calculator.java
+++ calculator.java
@@ -1,41 +1,9 @@
-import java.util.Scanner;
+package com.example.calculator;
 
-public class calculator {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double num1, num2, result;
-        char operator;
-
-        System.out.print("Enter the first number for add minus: ");
-        num1 = input.nextDouble();
-
-        System.out.print("Enter and choose the mention operator operator (+, -, *, /): ");
-        operator = input.next().charAt(0);
-
-        System.out.print("Enter the second number for add minus multiply: ");
-        num2 = input.nextDouble();
-
-        if (operator == '+') {
-            result = num1 + num2;
-        } else if (operator == '-') {
-            result = num1 - num2;
-        } else if (operator == '*') {
-            result = num1 * num2;
-        } else if (operator == '/') {
-            if (num2 != 0) {
-                result = num1 / num2;
-            } else {
-                System.out.println("Error: Division by zero is not allowed.");
-                return;
-            }
-        } else {
-            System.out.println("Error: this is the  Invalid operator.");
-            return;
-        }
-
-        System.out.println("Result: " + result);
+class Calculator
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
     }
-}fff}
-
-}}}
-}
+};
